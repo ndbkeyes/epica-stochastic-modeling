d03be872bb6471c4d59aaecaa3b415a73d8cb3dd
Nash Keyes <ndbkeyes@gmail.com>
2021-09-17 14:58:23 -0400

    Interactive plotting
    Realized that matplotlib does have interactive figures, if you run from command line. Wrote corresponding matplotlib code to plot original and interpolated data.

--- DataSet.py
+++ DataSet.py
@@ -7,7 +7,6 @@ Created on Tue Sep 14 11:56:55 2021
 
 import numpy as np
 import pandas as pd
-import plotly as pio
 import matplotlib.pyplot as plt
 import xarray as xr
 from scipy.interpolate import Akima1DInterpolator as akima
@@ -15,31 +14,31 @@ from scipy.interpolate import Akima1DInterpolator as akima
 
 class DataSet:
 
-    
+
 
     # constructor
     def __init__(self, name, path):
-        
-        
+
+
         ### READ & CLEAN DATA
         ts = pd.read_csv(path,sep='\s+')                                        # read CSV into pandas dataframe
         ts = ts.rename(columns={ts.columns[0]: "time", ts.columns[1]: "data"})  # rename columns to generic X, Y
         ts["time"] *= -1                                                        # make time axis values negative
         ts = ts.groupby(["time"],as_index=False).mean()                         # average Y-values with duplicate X-values
-        ts = ts.set_index("time",drop=True)                                     # make X into dimension not variable 
+        ts = ts.set_index("time",drop=True)                                     # make X into dimension not variable
         ts = ts.to_xarray()                                                     # convert pandas dataframe to xarray
-        
+
         ### SET ATTRIBUTES
         self.data_ts = ts
         self.nametag = name
 
-        
+
 
 
     # set up for modeling
     def model_setup(self,Pn,Pq):
-        
-        
+
+
         ### SETTINGS DICT
 
         sd = {}
@@ -51,35 +50,32 @@ class DataSet:
         sd["Py"] = sd["L"] / sd["Pn"]                           # length of period in years
         sd["Qn"] = sd["Pq"] * sd["Pn"]                          # number of points total
         sd["Qy"] = sd["L"] / sd["Qn"]                           # time between points in years
-        
+
         # timestamps of all period starts
         sd["P_times"] = np.linspace(sd["t_start"], sd["t_stop"], sd["Pn"], endpoint=False)
         # timestamps of all points - to interpolate to!
         sd["Q_times"] = np.linspace(sd["t_start"], sd["t_stop"], sd["Qn"], endpoint=False)
-        
-        self.divs = sd
-        
 
-        
+        self.divs = sd
+
+
+
         ### INTERPOLATE
-        f = akima( self.data_ts.time.values, self.data_ts.data.values )             # interpolator function                                          
+        f = akima( self.data_ts.time.values, self.data_ts.data.values )             # interpolator function
         ts_i = pd.DataFrame(data={"time": sd["Q_times"], "data": f(sd["Q_times"])}) # dataframe with interpolated time & data
         ts_i = ts_i.set_index("time",drop=True)                                     # index with time
         self.data_interp = ts_i.to_xarray()                                         # convert pandas to xarray
-        
-        # plot results
-        self.data_ts.plot.scatter(x="time",y="data")
-        self.data_interp.plot.scatter(x="time",y="data")
 
-        
+
+
 
         ### DATA MATRIX
-        
+
         # reshape time & data
         mat_shape = (sd["Pn"],sd["Pq"])
         time_reshape = np.reshape(self.data_interp.time.values, mat_shape)
         data_reshape = np.reshape(self.data_interp.data.values, mat_shape)
-   
+
         # new xarray for matrixed data & times, with coordinates (p,q)
         self.data_matrix = xr.DataArray(
             data=data_reshape,
@@ -90,10 +86,19 @@ class DataSet:
             ),
             dims=["p","q"]
         )
-        
-        
-        
-        
+
+
+
+
+        ### PLOTTING
+
+        self.data_ts.plot.scatter(x="time",y="data")
+        self.data_interp.plot.scatter(x="time",y="data")
+        plt.show()
+
+
+
+
 path = "C:/Users/ndbke/Dropbox/_NDBK/Research/epica_data/edc3/edc3-2008_co2_DATA-series3-composite.txt"
 ds = DataSet("co2",path)
 ds.model_setup(20,10)
